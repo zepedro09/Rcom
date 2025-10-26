c52e25c762edea9b8aeab571ff939f360b7868e5
Ze <zepedromf2005@gmail.com>
2025-10-26 11:32:01 +0000

    read parameters and opened file

--- src/application_layer.c
+++ src/application_layer.c
@@ -14,6 +14,36 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
 {
     LinkLayer link_layer;
     strcpy(link_layer.serialPort = serialPort);
-    if(strcmp("tx", role))
+    if(strcmp("tx", role) == 0){
+        link_layer.role = LlTx;
+    } else link_layer.role = LlRx;
+    link_layer.baudRate = baudRate;
+    link_layer.nRetransmissions = nTries;
+    link_layer.timeout = timeout;
+
+    if (llopen(link_layer) == -1) {
+        return -1;
+    }
+
+    if(link_layer.role == LlTx){
+        FILE *file = fopen(filename, "r");
+        if(file == NULL) {
+            printf("Can't find file \n")
+            return -1;
+        }
+
+    }
+
+
+    else if (link_layer.role == LlRx)
+    {
+    }
+    else return -1;
+
+
+    if (llclose(link_layer) == -1) {
+        return -1;
+    }
+    
 
 }
